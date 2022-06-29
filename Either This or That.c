dea56760b44071fac133af8ab92d31b5a185064a
AthulME <96113285+AthulME@users.noreply.github.com>
2022-06-29 22:48:40 +0530

    Either This or That

---
+++ Either This or That.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+ 
+    int i,n;
+    scanf("%d", &n);
+    for(i=0;i<=n;i++){
+        if(i%2==0&&i%3==0){
+            printf("%d \n", i);
+        }else if(i%2==0&&i%3==0&&i%5==0){
+            printf("%d \n", i);
+        }
+        
+    }   
+    return 0;
+}
