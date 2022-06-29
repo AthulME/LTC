a7459456cee30ec140037ba0714934b4c89e919e
AthulME <96113285+AthulME@users.noreply.github.com>
2022-06-29 22:26:28 +0530

    Varieties everywhere

---
+++ Varieties everywhere.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+    int a;
+    char b;
+    float c;
+    scanf("%d%*c%c%f",&a,&b,&c);
+    printf("%d\n%c\n%f",a,b,c);
+  
+
+       
+    return 0;
+}
