c2500daa9a6eac94cf3fe790a3ddb29cec6d20a2
AthulME <96113285+AthulME@users.noreply.github.com>
2022-06-29 22:33:34 +0530

    Do You Have the Capacity!!?

---
+++ Do You Have the Capacity!!?.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+float l,b,h;
+    scanf("%f%f%f",&l,&b,&h);
+    printf("%0.3f\n",(l*l*l));
+    printf("%0.3f",(l*b*h));
+      
+    return 0;
+}
