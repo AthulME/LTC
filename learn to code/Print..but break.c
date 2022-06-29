b0be88b7523d7366f322dbf758f0b4f85b579504
AthulME <96113285+AthulME@users.noreply.github.com>
2022-06-29 22:06:20 +0530

    Add files via upload

---
+++ learn to code/Print..but break.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hello\t");
+        printf("World \n");
+    printf("Welcome to Learn to code 2022");
+    
+    return 0;
+}
