26c6d3ce3bded4dfb8b5d31602193e46a3d5d62a
AthulME <96113285+AthulME@users.noreply.github.com>
2022-06-29 22:40:14 +0530

    STEPS OF 5

---
+++ STEPS OF 5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+  
+    int x;
+    for(x=11;x<99;x++){
+        if(x%5==0)
+        {
+            printf("%d\n",x);}
+        }
+        
+    return 0;
+}
